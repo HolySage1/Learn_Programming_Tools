9837004c75529746c95b56dc456b9a13429e2cd9
David-I-Termux-Ubuntu <kamsidavid2@gmail.com>
2025-05-12 16:03:40 +0100

    save

---
+++ Rust/project_0/target/debug/deps/project_0-e260aed300adcd7c.d
@@ -0,0 +1,5 @@
+/root/Learn_Programming_Tools/Rust/project_0/target/debug/deps/project_0-e260aed300adcd7c: src/main.rs
+
+/root/Learn_Programming_Tools/Rust/project_0/target/debug/deps/project_0-e260aed300adcd7c.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/project_0/target/debug/project_0.d
@@ -0,0 +1,1 @@
+/root/Learn_Programming_Tools/Rust/project_0/target/debug/project_0: /root/Learn_Programming_Tools/Rust/project_0/src/main.rs
